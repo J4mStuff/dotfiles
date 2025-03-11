239829aaff000f9800462bc7ad3ff47d283bb007
J4mStuff <59264088+J4mStuff@users.noreply.github.com>
2025-03-11 13:12:58 +0100

    update formatters for nvim

--- dot-config/nvim/lua/plugins/autoformat.lua
+++ dot-config/nvim/lua/plugins/autoformat.lua
@@ -2,21 +2,50 @@
 return {
   'stevearc/conform.nvim',
   opts = {
+    log_level = vim.log.levels.DEBUG,
     notify_on_error = true,
+    notify_no_formatters = true,
     format_on_save = {
       timeout_ms = 500,
       lsp_fallback = true,
     },
+    lang_to_ext = {
+      sh = 'zsh',
+      c_sharp = 'cs',
+      javascript = 'js',
+      markdown = 'md',
+      python = 'py',
+      rust = 'rs',
+      c = 'c',
+      zig = 'zig',
+    },
+    lang_to_formatters = {},
     formatters_by_ft = {
       lua = { 'stylua' },
-      bash = { 'beautysh' },
-      python = { 'blue' },
-      zig = { 'zig fmt' },
+      c = { 'clang-format' },
+      sh = { 'beautysh' },
+      py = { 'blue' },
+      zig = { 'zig_fmt' },
       yaml = { 'yq' },
-      json = { 'yq' },
-      cs = { 'dotnet csharpier' },
-      css = { 'js-beautify' },
+      json = { 'jq' },
+      cs = { 'cs_fmt' },
+      css = { 'css_fmt' },
       toml = { 'taplo' },
+      ['_'] = { 'trim_whitespace' },
+    },
+    formatters = {
+      css_fmt = {
+        command = 'css-beautify',
+      },
+      cs_fmt = {
+        command = 'dotnet',
+        prepend_args = { 'csharpier' },
+      },
+      zig_fmt = {
+        command = 'zig',
+        prepend_args = { 'fmt' },
+        inherit = false,
+      },
     },
   },
 }
