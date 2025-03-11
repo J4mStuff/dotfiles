b275ad3c61bdbd85fbad1c0ebda1aca23982888a
J4mStuff <59264088+J4mStuff@users.noreply.github.com>
2025-03-11 11:54:43 +0100

    adding lsp and formatters

--- dot-config/nvim/lua/plugins/autoformat.lua
+++ dot-config/nvim/lua/plugins/autoformat.lua
@@ -2,13 +2,21 @@
 return {
   'stevearc/conform.nvim',
   opts = {
-    notify_on_error = false,
+    notify_on_error = true,
     format_on_save = {
       timeout_ms = 500,
       lsp_fallback = true,
     },
     formatters_by_ft = {
       lua = { 'stylua' },
+      bash = { 'beautysh' },
+      python = { 'blue' },
+      zig = { 'zig fmt' },
+      yaml = { 'yq' },
+      json = { 'yq' },
+      cs = { 'dotnet csharpier' },
+      css = { 'js-beautify' },
+      toml = { 'taplo' },
     },
   },
 }
