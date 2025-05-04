9f7bc12454309393ac75f02d8010f8e277db6a41
J4mStuff <59264088+J4mStuff@users.noreply.github.com>
2025-05-04 12:39:28 +0200

    fix a bug with cancelled search lookup

--- dot-local/bin/scripts/fuzzel_search.sh
+++ dot-local/bin/scripts/fuzzel_search.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/sh
 
 search_string=$(fuzzel --dmenu --lines 0 --placeholder "Type your search" | sed 's/ /+/g')
+
+if [[ -z "$search_string" ]]; then
+    exit
+fi
+
 librewolf --new-tab "https://kagi.com/search?q=$search_string"
+
