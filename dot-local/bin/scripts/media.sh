d5e35c6026ffa046239a51e83c80db0665d819a4
J4mStuff <59264088+J4mStuff@users.noreply.github.com>
2025-07-20 00:26:52 +0200

    cleanup and update media script

--- dot-local/bin/scripts/media.sh
+++ dot-local/bin/scripts/media.sh
@@ -3,7 +3,7 @@
 player_status=$(playerctl --player=spotify_player status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo "$(playerctl --player=spotify_player metadata artist) - $(playerctl --player=spotify_player metadata title)"
+    echo "$(playerctl --ignore-player firefox metadata artist) - $(playerctl --ignore-player firefox metadata title)"
 elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl --player=spotify_player metadata artist) - $(playerctl --player=spotify_player metadata title)"
+    echo " $(playerctl --ignore-player firefox metadata artist) - $(playerctl --ignore-player firefox metadata title)"
 fi
