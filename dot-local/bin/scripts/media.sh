0cb232d793ebe5577b2b3b4f02841fd30c660ad1
J4mStuff <59264088+J4mStuff@users.noreply.github.com>
2025-07-19 23:53:17 +0200

    media keys and updated spotify_player script

--- dot-local/bin/scripts/media.sh
+++ dot-local/bin/scripts/media.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-player_status=$(playerctl --player=spotify status 2> /dev/null)
+player_status=$(playerctl --player=spotify_player status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo "$(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
+    echo "$(playerctl --player=spotify_player metadata artist) - $(playerctl --player=spotify_player metadata title)"
 elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
+    echo " $(playerctl --player=spotify_player metadata artist) - $(playerctl --player=spotify_player metadata title)"
 fi
