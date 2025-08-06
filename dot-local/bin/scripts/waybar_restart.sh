7893dda50abb4977272d060be5edfdcfe5c0c52b
J4mStuff <59264088+J4mStuff@users.noreply.github.com>
2025-08-06 09:34:58 +0200

    fix stuff

--- dot-local/bin/scripts/waybar_restart.sh
+++ dot-local/bin/scripts/waybar_restart.sh
@@ -2,7 +2,7 @@
 
 set -a; source ~/.cache/wal/colors.sh; set +a
 
-killall -SIGUSR2 waybar
+killall -SIGUSR2 waybar || waybar
 
 hyprctl keyword general:col.active_border "rgba(${color1:1}ee)" "rgba(${color2:1}ee)" "45deg"
 hyprctl keyword general:col.inactive_border "rgba(${color3:1}ff)"
